aca7a528b3fe6b96201a4c539e51ac2e49dddedd
Javier Aragones <javaragones@gmail.com>
2025-03-14 19:21:16 -0300

    Upload files

---
+++ scripts/sway.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#
+#exec ck-launch-session dbus-launch --sh-syntax --exit-with-session sway
+#
+# IMPORTANTE: Para que funcione pipewire por este método, debe estar inhabilitado en el sistema (pipewire-disable.sh).
+#
+/usr/bin/daemon -rB --pidfiles=~/.run --name=pipewire /usr/bin/pipewire 
+/usr/bin/daemon -rB --pidfiles=~/.run --name=pipewire-pulse /usr/bin/pipewire-pulse
+/usr/bin/daemon -rB --pidfiles=~/.run --name=wireplumber /usr/bin/wireplumber
+
+exec dbus-launch --sh-syntax --exit-with-session sway
